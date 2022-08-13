c519c06c6e31ce0f2c1b6a8ad247113f4304963e
nimaprmdi <nimaprmdi@gmail.com>
2022-08-13 15:09:51 +0430

    Literal types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-// Intersection types
-type Draggable = {
-    darg: () => void;
-};
+// Literal types
+type Quantity = 50 | 100; // We can assign just 50 or 100
+let quantity: Quantity = 50; // Defining a quantity type
 
-type Resizable = {
-    resize: () => void;
-};
-
-type UiWidgets = Draggable & Resizable;
-
-let userWidget: UiWidgets = {
-    darg: () => {},
-    resize: () => {},
-};
+// Using String type
+type Metric = "cm" | "inch";
