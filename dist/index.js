7b3441367f763a3e1e7f9c37a3745fc05029088a
nimaprmdi <nimaprmdi@gmail.com>
2022-08-13 15:39:06 +0430

    Optional Chaining

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,12 @@
 "use strict";
-let userWidget = {
-    darg: () => { },
-    resize: () => { },
-};
+var _a;
+function getCustomer(id) {
+    return id === 0 ? null : { birthDay: new Date(), userId: id };
+}
+let customer = getCustomer(2);
+// (customer?) will say if (customer) is NOT null and is NOT undefined
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthDay);
+// (customer?.birthDay?) will say if both of them NOT null and is NOT undefined
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthDay) === null || _a === void 0 ? void 0 : _a.getFullYear());
+console.log(customer === null || customer === void 0 ? void 0 : customer.userId);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-// Literal types
-type Quantity = 50 | 100; // We can assign just 50 or 100
-let quantity: Quantity = 50; // Defining a quantity type
+// Optional Chaining
+type Customer = {
+    birthDay: Date;
+    userId: number; // Just Here chilling
+};
 
-// Using String type
-type Metric = "cm" | "inch";
+function getCustomer(id: number): Customer | null {
+    return id === 0 ? null : { birthDay: new Date(), userId: 0 }; // Should be two of them (birthDay & userId)
+}
+
+let customer = getCustomer(2);
+
+// (customer?) will say if (customer) is NOT null and is NOT undefined
+console.log(customer?.birthDay);
+
+// (customer?.birthDay?) will say if both of them NOT null and is NOT undefined
+console.log(customer?.birthDay?.getFullYear());
+
+let log: any = null;
+log?.("a"); // Will execute if it reference a function
