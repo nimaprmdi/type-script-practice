f172a14ad4ce484998f0d70eaab47b46202c811a
nimaprmdi <nimaprmdi@gmail.com>
2022-08-13 15:51:12 +0430

    Nullish Operator

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,9 @@
 "use strict";
-var _a;
-function getCustomer(id) {
-    return id === 0 ? null : { birthDay: new Date(), userId: id };
-}
-let customer = getCustomer(2);
-// (customer?) will say if (customer) is NOT null and is NOT undefined
-console.log(customer === null || customer === void 0 ? void 0 : customer.birthDay);
-// (customer?.birthDay?) will say if both of them NOT null and is NOT undefined
-console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthDay) === null || _a === void 0 ? void 0 : _a.getFullYear());
-console.log(customer === null || customer === void 0 ? void 0 : customer.userId);
+let speed = -2;
+let ride = {
+    // Nullish Operator
+    // (??) will say if value was true put there (speed) otherwise put 30
+    speed: speed !== null && speed !== void 0 ? speed : 30,
+};
+console.log(ride); // if null give 30 if (0) give (0) if (other value) give (other value)
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
-// Optional Chaining
-type Customer = {
-    birthDay: Date;
-    userId: number; // Just Here chilling
+let speed: number | null = -2;
+
+let ride = {
+    // Nullish Operator
+    // (??) will say if value was true put there (speed) otherwise put 30
+    speed: speed ?? 30,
 };
 
-function getCustomer(id: number): Customer | null {
-    return id === 0 ? null : { birthDay: new Date(), userId: 0 }; // Should be two of them (birthDay & userId)
-}
-
-let customer = getCustomer(2);
-
-// (customer?) will say if (customer) is NOT null and is NOT undefined
-console.log(customer?.birthDay);
-
-// (customer?.birthDay?) will say if both of them NOT null and is NOT undefined
-console.log(customer?.birthDay?.getFullYear());
-
-let log: any = null;
-log?.("a"); // Will execute if it reference a function
+console.log(ride); // if null give 30 if (0) give (0) if (other value) give (other value)
