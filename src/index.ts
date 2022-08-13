ebf0053c25c7b5467781ddc8ddd8e8caf9544bdf
nimaprmdi <nimaprmdi@gmail.com>
2022-08-13 11:20:01 +0430

    Add Union Type checking { number | string }

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,6 @@
-let sales: number = 123_456; // Type number
-let course: string = "String"; // Type string
-let is_published = false; // Type boolean (Also we can dont mention the variable type)
-let level; // Type Any
-let numbers: number[] = [1, 2, 3];
+// Union Type Checking
 
-let user: [number, string] = [1, "Nima"];
-user.push(1);
-
-// PascalCase
-const enum Size {
-    Small = 1,
-    Medium, // Automaticaly set 2
-    Large, // Automaticaly set 3
-}
-
-// Like this we can define own type and value
-const myUser: Size = Size.Medium;
-
-type Employee = {
-    readonly id: number;
-    name?: string; // optional value
-    retire: (date: Date) => void;
-};
-
-let employee: Employee = {
-    id: 1,
-    name: "Nima",
-    retire: () => {
-        console.log(Date);
-    },
-};
-
-// Giving instant, optional, default arguments
-function calculateTax(income: number, taxYear?: number, taxYears = 2022): number {
-    if ((taxYear || 2022) < 50_000) return income * 1.2;
-    return income * 1.3 + taxYears;
+function kgToLbs(weight: number | string): number {
+    if (typeof weight === "number") return weight * 2.2; // Intellisense  detect that is number
+    else return parseInt(weight) * 2.2; // Intellisense detect that is string
 }
