5db87b40951fcfe8db3f9ce9e1e6ee725cfc6f92
nimaprmdi <nimaprmdi@gmail.com>
2022-08-14 16:42:38 +0430

    Inhertiance

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,19 @@
-class Ride {
-    // Using (static) we say we want (_activeRides) belong to the class not objects
-    static _activeRides: number = 0;
+class Person {
+    constructor(public firstname: string, public lastname: string) {}
 
-    start() {
-        Ride._activeRides++;
+    get fullName(): string {
+        return this.firstname + " " + this.lastname;
     }
 
-    stop() {
-        Ride._activeRides--;
-    }
-    // with defining static we say _activeRides belongs to Ride class not the object
-    static get activeRides(): number {
-        return Ride._activeRides;
+    walk() {
+        console.log("Walk");
     }
 }
 
-let ride1 = new Ride(); // This will create an object
-let ride2 = new Ride();
+class Student extends Person {
+    constructor(public studentId: number, firstName: string, lastName: string) {
+        super(firstName, lastName);
+    }
+}
 
-ride1.start();
-
-ride2.start();
-
-console.log(Ride.activeRides); // this will read from Ride Class (Liek global place)
+let student = new Student(1, "Nima", "Prmi");
