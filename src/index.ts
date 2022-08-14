56a987f7f01453de68484b6ec4d4b48accc8fb71
nimaprmdi <nimaprmdi@gmail.com>
2022-08-14 09:08:32 +0430

    Never type

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
-// Using (unknown) is prefred instead of any (Give us better type checking)
-function render(document: any) {
-    // This is equal to if(typeof document === 'string').
-    // But with instance of we can say custom objet
-    if (document instanceof WordDocument) { // WordDocument is (type) for example
-        document.toUpperCase();
-    }
-
-    // type of just work for `string` , `boolean` , `number`
-    if (typeof document === "string") {
-        document.toUpperCase();
+function processEvent(): never {
+    while (true) {
+        // Infinite Loop (Never Returns)
     }
 }
+
+processEvent();
+console.log("hello World");
