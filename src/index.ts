814e0f116ab69f8e81d925b1214a0077b6d86f6a
nimaprmdi <nimaprmdi@gmail.com>
2022-08-16 13:51:57 +0430

    generic functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-// Will say that we should defie type when we are creating the class
-class keyValuePairs<K, V> {
-    constructor(public key: K, public value: V) {}
+class ArrayUtils {
+    static wrapInArray<S>(value: S) {
+        return [value];
+    }
 }
 
-// Type 1: Defining type using <TYPE, TYPE>
-let pair = new keyValuePairs<string, string>("1", "1");
-// Type 2: Atomatic difinition
-let pairs_2 = new keyValuePairs("2", "2");
+// A functional (outside of the class)
+function wrapInArray<T>(value: T) {
+    return [value];
+}
+// From The function
+const arr = wrapInArray("1");
+// In the class
+const arrUtil = ArrayUtils.wrapInArray(1);
