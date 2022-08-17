e6935115d8f1828dd9ca006333aadb051f9f6350
nimaprmdi <nimaprmdi@gmail.com>
2022-08-17 11:20:14 +0430

    component decorator

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,20 @@
 "use strict";
-let product = {
-    name: "a",
-    price: 5,
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-product.name = "5";
-console.log(product.name);
-/** Next level Readonly */
+function Component(constructor) {
+    console.log("Component decorator called");
+    constructor.prototype.userId = Date.now();
+    constructor.prototype.insertInDom = () => {
+        console.log("Inserting dom component in dom ");
+    };
+}
+let ProfileComponent = class ProfileComponent {
+};
+ProfileComponent = __decorate([
+    Component
+], ProfileComponent);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,11 @@
-interface Product {
-    name: string;
-    price: number;
+function Component(constructor: Function) {
+    console.log("Component decorator called");
+
+    constructor.prototype.userId = Date.now();
+    constructor.prototype.insertInDom = () => {
+        console.log("Inserting dom component in dom ");
+    };
 }
 
-// This will create anything in the product interface readonly
-type ReadOnlyProducts = {
-    readonly // (K) is the key => it is like a for loop (This just work for Product interface)
-    [K in keyof Product]: Product[K];
-};
-
-let product: ReadOnlyProducts = {
-    name: "a",
-    price: 5,
-};
-// product.name = "5"; // here we can not change the value of the name will get an error
-
-/******** Next level Readonly *********/
-type Optional<T> = {
-    // using (?) made it optional
-    [K in keyof T]?: T[K]; // made every thing here optional
-};
-
-type Nullable<T> = {
-    [K in keyof T]: T[K] | null;
-};
-
-let optional: Optional<Product> = {
-    name: "name",
-    // price is optional here
-};
-
-let nullable: Nullable<Product> = {
-    name: null,
-    price: null,
-};
+@Component
+class ProfileComponent {}
